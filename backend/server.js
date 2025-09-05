345acb3f724fd5818595fcef3a2383b742b7d4a4
Akshitha947 <akshithach83@gmail.com>
2025-09-05 21:19:01 +0530

    Configure CORS for production

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 require('dotenv').config();
-const connectDB = require('./config/db'); 
+const connectDB = require('./config/db');
 const cors = require('cors'); 
+const path = require('path');
 
 const app = express();
+connectDB();
 
 
-connectDB(); 
-app.use(cors());
-
+const corsOptions = {
+  origin: 'https://stupendous-smakager-127e92.netlify.app'
+};
+app.use(cors(corsOptions));
 
 
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 5000;
-
-
-
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
