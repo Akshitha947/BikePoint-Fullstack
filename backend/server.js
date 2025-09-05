53041f70fd7ca95e178ff9911ab679117ff55e74
Akshitha947 <akshithach83@gmail.com>
2025-09-05 21:40:14 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,20 @@ const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 connectDB();
 
-
-const whitelist = ['http://stupendous-smakager-127e92.netlify.app'];
 const corsOptions = {
-  origin: function (origin, callback) {
-    
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      
-      callback(null, true);
-    } else {
-     
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: 'https://stupendous-smakager-127e92.netlify.app'
 };
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
-
 
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
